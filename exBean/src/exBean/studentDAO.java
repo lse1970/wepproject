3a413d0e0e4a8631315b3b1e3d4ff30f193ad04f
D7701 <D7701@DESKTOP-ODBNK9S>
2018-09-27 15:38:33 +0900

    good

--- exBean/src/exBean/studentDAO.java
+++ exBean/src/exBean/studentDAO.java
@@ -1,25 +1,40 @@
-package exBean;
+ package exBean;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 public class studentDAO {
  public void insertStudent(studentVO item) throws Exception {
+    
   String no = item.getNo();
   String name = item.getName();
-
-
+  
   Class.forName("oracle.jdbc.driver.OracleDriver");
   Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","201710552","201710552");
-
- 
-
   PreparedStatement pstmt = conn.prepareStatement("insert into student values(?,?)");
   pstmt.setString(1,no);
   pstmt.setString(2,name);
   pstmt.executeUpdate();
-
- 
-
   pstmt.close();
-
   conn.close();
  }
+
+ public List<studentVO> selectStudent() throws Exception { //List에 record set을 담아 넘겨 준다
+      List<studentVO> items = new ArrayList<studentVO>();
+      Class.forName("oracle.jdbc.driver.OracleDriver");
+      
+      Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","201710552","201710552"); // DB 접속
+      PreparedStatement pstmt = conn.prepareStatement("select * from student");
+      ResultSet rs = pstmt.executeQuery();// SQL문 실행
+
+      while(rs.next()) {
+         studentVO item = new studentVO();
+         item.setNo(rs.getString("no"));
+         item.setName(rs.getString("name"));
+         items.add(item);
+      } 
+      pstmt.close();
+      rs.close();
+      conn.close();
+      return items;
+    }
 }
